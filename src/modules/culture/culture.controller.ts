c23ab7d09d1667bed9558dd11f00248b8e94b3ab
Daniel Teófilo <50593453+DanielTeofilo97@users.noreply.github.com>
2024-09-13 18:47:22 -0300

    feat: response json example culture swagger

--- src/modules/culture/culture.controller.ts
+++ src/modules/culture/culture.controller.ts
@@ -7,11 +7,14 @@ import { ApiBearerAuth, ApiHeader, ApiOperation, ApiQuery, ApiResponse, ApiTags 
 import { CreateCultureDTO } from './dto/create-culture.dto';
 import { Roles } from '../../decorators/role.decorator';
 import { Role } from '../../enums/role.enum';
+import * as cultureCreateExamples from './examples/culture-create.example.json';
+import * as culturelistExamples from './examples/culture-list.example.json';
+import * as cultureDeleteExamples from './examples/culture-delete.example.json';
 
 @ApiBearerAuth()
 @ApiHeader({
-  name: 'Authorization',
-  description: 'Bearer token',
+    name: 'Authorization',
+    description: 'Bearer token',
 })
 @Roles(Role.Admin)
 @UseGuards(AuthGuard, RoleGuard)
@@ -29,18 +32,41 @@ export class CultureController {
     @UseGuards(AuthGuard, RoleGuard)
     @Post()
     @ApiOperation({ summary: 'Criar Cultura' })
-    @ApiResponse({ status: 201, description: 'Cultura criada' })
+    @ApiResponse({
+        status: 201,
+        description: 'Cultura criada',
+        content: {
+            'application/json': {
+                example: cultureCreateExamples['201']
+            }
+        }
+    })
     @ApiResponse({
         status: 400,
         description: 'Erro durante a criação da cultura',
+        content: {
+            'application/json': {
+                example: cultureCreateExamples['400']
+            }
+        }
     })
     @ApiResponse({
         status: 409,
-        description: 'Cultura já cadastrado',
+        description: 'Culture com esse nome já existe',
+        content: {
+            'application/json': {
+                example: cultureCreateExamples['409']
+            }
+        }
     })
     @ApiResponse({
         status: 403,
         description: 'Usuario não tem acesso ao recurso',
+        content: {
+            'application/json': {
+                example: cultureCreateExamples['403']
+            }
+        }
     })
     @ApiTags('cultures')
     async create(@Body() data: CreateCultureDTO, @Req() req) {
@@ -50,14 +76,32 @@ export class CultureController {
 
 
     @ApiOperation({ summary: 'Listagem Culturas' })
-    @ApiResponse({ status: 200, description: 'Listagem  Culturas' })
+    @ApiResponse({
+        status: 200,
+        description: 'Listagem  Culturas',
+        content: {
+            'application/json': {
+                example: culturelistExamples['200']
+            }
+        }
+    })
     @ApiResponse({
         status: 404,
         description: 'Nenhuma cultura encontrado',
+        content: {
+            'application/json': {
+                example: culturelistExamples['404']
+            }
+        }
     })
     @ApiResponse({
         status: 400,
         description: 'Erro ao buscar culturas',
+        content: {
+            'application/json': {
+                example: culturelistExamples['400']
+            }
+        }
     })
     @ApiQuery({ name: 'skip', required: false, description: 'Quantidade de registros para pular', example: 0 })
     @ApiQuery({ name: 'take', required: false, description: 'Quantidade de registros para pegar', example: 20 })
@@ -95,15 +139,29 @@ export class CultureController {
     @Roles(Role.Admin)
     @UseGuards(AuthGuard, RoleGuard)
     @ApiOperation({ summary: 'Desabilitar Cultura' })
-    @ApiResponse({ status: 200, description: 'Cultura desabilitado.' })
-    @ApiResponse({ status: 404, description: 'Cultura não encontrada.' })
     @ApiResponse({
-      status: 403,
-      description: 'Usuario não tem acesso ao recurso',
+        status: 200, description: 'Cultura desabilitado.'
+    })
+    @ApiResponse({
+        status: 404, description: 'Cultura não encontrada.',
+        content: {
+            'application/json': {
+                example: cultureDeleteExamples['404']
+            }
+        }
+    })
+    @ApiResponse({
+        status: 403,
+        description: 'Usuario não tem acesso ao recurso',
+        content: {
+            'application/json': {
+                example: cultureDeleteExamples['403']
+            }
+        }
     })
     @ApiTags('cultures')
     @Delete(':id')
     async delete(@Param('id', ParseUUIDPipe) id: string) {
-      return this.cultureService.delete(id);
+        return this.cultureService.delete(id);
     }
 }
