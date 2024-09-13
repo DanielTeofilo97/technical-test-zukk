20c4c90c0d99802c1147dd6d0bfdeaa171b3afa6
Daniel Teófilo <50593453+DanielTeofilo97@users.noreply.github.com>
2024-09-13 10:59:19 -0300

    fix: fix validation

--- src/modules/producer/validator/area-total.validator.ts
+++ src/modules/producer/validator/area-total.validator.ts
@@ -4,10 +4,19 @@ import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments 
 export class IsValidAreaTotal implements ValidatorConstraintInterface {
   validate(areaTotal: number, args: ValidationArguments) {
     const { areaAgricultavelHectares, areaVegetacaoHectares } = args.object as any;
-    return areaTotal >= areaAgricultavelHectares + areaVegetacaoHectares;
+    const somaAreas = areaAgricultavelHectares + areaVegetacaoHectares;
+    return areaTotal >= somaAreas && areaTotal <= somaAreas; 
   }
 
-  defaultMessage() {
-    return 'A soma da área agricultável e vegetação não pode ser maior que a área total.';
+  defaultMessage(args: ValidationArguments) {
+    const { areaAgricultavelHectares, areaVegetacaoHectares } = args.object as any;
+    const somaAreas = areaAgricultavelHectares + areaVegetacaoHectares;
+
+    if (args.value < somaAreas) {
+      return `A área total (${args.value} hectares) não pode ser menor que a soma das áreas agricultável e vegetação (${somaAreas} hectares).`;
+    } else if (args.value > somaAreas) {
+      return `A área total (${args.value} hectares) não pode ser maior que a soma das áreas agricultável e vegetação (${somaAreas} hectares).`;
+    }
+    return 'A área total está incorreta.';
   }
 }
